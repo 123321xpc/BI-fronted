e0ef409a80fcb09322443ba6731e92a60102014c
xupeicai <2533681677@qq.com>
2025-06-13 09:08:29 +0800

    add form modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,6 @@
 import { MyFlex } from '@/components/MyFlex';
-import { getLoginUserUsingGet } from '@/service/api/userController';
 
 const HomePage: React.FC = () => {
-  getLoginUserUsingGet().then((res) => {
-    console.log(res);
-  });
   return <MyFlex></MyFlex>;
 };
 
