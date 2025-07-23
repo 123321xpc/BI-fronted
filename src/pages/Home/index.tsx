74d0588daf3f1602d6c2860b68e1a674413606ad
xupeicai <2533681677@qq.com>
2025-07-23 16:07:00 +0800

    feat: form modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,66 +1,7 @@
-import FormModal, { FormModalRef } from '@/components/FormModal';
 import MyFlex from '@/components/MyFLex';
-import QuickForm from '@/components/QuickForm';
-import { useAllSearchParams } from '@/hooks/useAllSearchParams';
-import { Button } from 'antd';
-import { useCallback, useRef } from 'react';
 
 const HomePage: React.FC = () => {
-  useAllSearchParams();
-  const modalRef = useRef<FormModalRef>(null);
-  const handleEdit = useCallback(() => {
-    console.log('handleEdit', modalRef.current);
-    modalRef.current?.share({ name: '张三', gender: 'Male' }, 'edit');
-  }, [modalRef]);
-
-  return (
-    <MyFlex vertical>
-      <FormModal
-        trigger={'创建用户'}
-        objName={'用户'}
-        onSubmit={(form) => {
-          console.log('onSubmit', form.getFieldsValue());
-          return false;
-        }}
-        operatingType={'create'}
-        ref={modalRef}
-        schema={{
-          name: {
-            label: 'Name',
-          },
-          gender: {
-            label: 'Gender',
-            type: 'select',
-            options: [
-              { value: 'Male', label: 'Male' },
-              { value: 'Female', label: 'Female' },
-              { value: 'Other', label: 'Other' },
-            ],
-          },
-        }}
-      />
-      <Button type="primary" onClick={handleEdit}>
-        edit用户
-      </Button>
-      <QuickForm
-        schema={{
-          name: {
-            label: 'Name',
-            rules: [{ required: true, message: 'Please input your name!' }],
-          },
-          gender: {
-            label: 'Gender',
-            type: 'select',
-            options: [
-              { value: 'Male', label: 'Male' },
-              { value: 'Female', label: 'Female' },
-              { value: 'Other', label: 'Other' },
-            ],
-          },
-        }}
-      />
-    </MyFlex>
-  );
+  return <MyFlex vertical>home page</MyFlex>;
 };
 
 export default HomePage;
